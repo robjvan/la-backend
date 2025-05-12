1dc59f21d2a97747686c0605ca4457018ee5c1f7
Rob Vandelinder <robjvan@gmail.com>
2025-05-11 21:42:08 -0230

    Added global API prefix, cors, global validation, swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  /// Add global prefix to all endpoints
+  app.setGlobalPrefix('api/v1');
+
+  /// Handle all user input validation globally
+  app.useGlobalPipes(new ValidationPipe()); // Set the config options
+  app.enableCors();
+
+  // Set Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('LeafAlert Platform')
+    .setDescription('LeafAlert platform API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.API_PORT || 3000);
 }
 bootstrap();
