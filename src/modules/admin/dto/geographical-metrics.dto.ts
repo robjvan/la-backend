decb414bc2d0dce24fb5be33fc0ed05bfedeef7b
Rob Vandelinder <robjvan@gmail.com>
2025-05-17 21:55:51 -0230

    Added dto fields

--- src/modules/admin/dto/geographical-metrics.dto.ts
+++ src/modules/admin/dto/geographical-metrics.dto.ts
@@ -1,1 +1,14 @@
-export class GeographicalMetricsDto {}
+export class GeographicalMetricsDto {
+  usersInCanada: number;
+  usersInUSA: number;
+  usersInOther: number;
+
+  topCountryByUsers: number;
+  topCountryNumUsers: number;
+  topCityByUsers: number;
+  topCityNumUsers: number;
+  topCountryByLogins: number;
+  topCountryNumLogins: number;
+  topCityByLogins: number;
+  topCityNumLogins: number;
+}
