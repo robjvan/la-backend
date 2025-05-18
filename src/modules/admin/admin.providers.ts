1f5de9fe51bba55e554da73e53cfbcd614370c27
Rob Vandelinder <robjvan@gmail.com>
2025-05-18 20:23:57 -0230

    Added repo providers

--- src/modules/admin/admin.providers.ts
+++ src/modules/admin/admin.providers.ts
@@ -1,13 +1,21 @@
 import {
+  CITY_REPOSITORY,
+  COUNTRY_REPOSITORY,
+  FERTILIZER_RECORDS_REPOSITORY,
   PLANT_REPOSITORY,
   USER_LOGIN_RECORD_REPOSITORY,
   USER_PROFILE_REPOSITORY,
   USER_REPOSITORY,
+  WATERING_RECORDS_REPOSITORY,
 } from 'src/constants';
 import { UserLoginRecordModel } from '../auth/models/user-login-record.model';
 import { UserModel } from '../users/models/user.model';
 import { PlantModel } from '../plants/models/plant.model';
 import { UserProfileModel } from '../user-profiles/models/user-profile.model';
+import { FertilizerRecordModel } from '../plants/models/fertilizer-record.model';
+import { WateringRecordModel } from '../plants/models/watering-record.model';
+import { CityModel } from '../locations/models/city.model';
+import { CountryModel } from '../locations/models/country.model';
 
 export const adminProviders = [
   {
@@ -25,5 +33,21 @@ export const adminProviders = [
   {
     provide: USER_PROFILE_REPOSITORY,
     useValue: UserProfileModel,
+  },
+  {
+    provide: WATERING_RECORDS_REPOSITORY,
+    useValue: WateringRecordModel,
+  },
+  {
+    provide: FERTILIZER_RECORDS_REPOSITORY,
+    useValue: FertilizerRecordModel,
+  },
+  {
+    provide: COUNTRY_REPOSITORY,
+    useValue: CountryModel,
+  },
+  {
+    provide: CITY_REPOSITORY,
+    useValue: CityModel,
   },
 ];
