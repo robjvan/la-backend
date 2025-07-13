2ff0dc769a2b836922c055302ed7c7de013dce17
Rob Vandelinder <robjvan@gmail.com>
2025-07-13 10:31:38 -0230

    Added fields

--- src/modules/plants/dto/new-plant.dto.ts
+++ src/modules/plants/dto/new-plant.dto.ts
@@ -1,4 +1,79 @@
+import {
+  IsArray,
+  IsBoolean,
+  IsDate,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
 export class NewPlantDto {
+  @IsNumber()
+  @IsNotEmpty()
   userId: number;
-  // TODO(RV): Add fields
+
+  @IsString()
+  @IsOptional()
+  name?: string;
+
+  @IsNumber()
+  @IsOptional()
+  waterIntervalDays?: number;
+
+  @IsNumber()
+  @IsOptional()
+  fertilizerIntervalDays?: number;
+
+  @IsDate()
+  @IsOptional()
+  lastWateredAt?: Date;
+
+  @IsDate()
+  @IsOptional()
+  lastFertilizedAt?: Date;
+
+  @IsBoolean()
+  @IsOptional()
+  reminderEnabled: boolean;
+
+  @IsBoolean()
+  @IsOptional()
+  fertilizerReminderEnabled: boolean;
+
+  @IsArray()
+  @IsOptional()
+  tags: string[] = [];
+
+  @IsArray()
+  @IsOptional()
+  notes: string[] = [];
+
+  @IsString()
+  @IsOptional()
+  location?: string;
+
+  @IsString()
+  @IsOptional()
+  humidityPreference?: string;
+
+  @IsString()
+  @IsOptional()
+  sunlightPreference?: string;
+
+  @IsString()
+  @IsOptional()
+  imageUrl?: string;
+
+  @IsString()
+  @IsOptional()
+  species?: string;
+
+  @IsString()
+  @IsOptional()
+  soilType?: string;
+
+  @IsNumber()
+  @IsOptional()
+  waterAmount?: number;
 }
