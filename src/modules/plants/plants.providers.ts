f95403f4513871bf56ae86db7944eb5d3a432146
Rob Vandelinder <robjvan@gmail.com>
2025-07-14 22:19:06 -0230

    Added models to providers

--- src/modules/plants/plants.providers.ts
+++ src/modules/plants/plants.providers.ts
@@ -1,9 +1,23 @@
-import { PLANT_REPOSITORY } from 'src/constants';
+import {
+  FERTILIZER_RECORDS_REPOSITORY,
+  PLANT_REPOSITORY,
+  WATERING_RECORDS_REPOSITORY,
+} from 'src/constants';
 import { PlantModel } from './models/plant.model';
+import { WateringRecordModel } from './models/watering-record.model';
+import { FertilizerRecordModel } from './models/fertilizer-record.model';
 
 export const plantsProviders = [
   {
     provide: PLANT_REPOSITORY,
     useValue: PlantModel,
+  },
+  {
+    provide: WATERING_RECORDS_REPOSITORY,
+    useValue: WateringRecordModel,
+  },
+  {
+    provide: FERTILIZER_RECORDS_REPOSITORY,
+    useValue: FertilizerRecordModel,
   },
 ];
